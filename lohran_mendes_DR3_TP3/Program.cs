e68c59231d32636a7caf17f750303fa6a6da50c4
Lohran Mendes <lohranfm@gmail.com>
2025-06-09 09:42:18 -0300

    initial commit

---
+++ lohran_mendes_DR3_TP3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace lohran_mendes_DR3_TP3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Carro.Exercicio_1();
+        // Ingresso.Exercicio_4();
+        Ingresso.Exercicio_5();
+    }
+}
